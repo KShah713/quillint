c1b25d7a203df5d87cd088b3a1d14d319b4d01bd
KhushiShah01 <khushi.bharat.shah@gmail.com>
2021-05-17 20:52:17 +0530

    Added firebase backend

--- quillint/lib/main.dart
+++ quillint/lib/main.dart
@@ -1,37 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:quillint/screens/wrapper.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: Wrapper(),
   ));
 }
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(30),
-          child: Column(
-            children: [
-              SizedBox(height:250),
-              Text(
-                'QUILLINT',
-                style: TextStyle(
-                  color: Colors.amber[600],
-                  letterSpacing: 2,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                  fontFamily: 'Caveat',
-                ),
-              ),
-              Spacer(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// 
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey[900],
+//       body: SafeArea(
+//         child: Padding(
+//           padding: EdgeInsets.all(30),
+//           child: Column(
+//             children: [
+//               SizedBox(height:250),
+//               Text(
+//                 'QUILLINT',
+//                 style: TextStyle(
+//                   color: Colors.amber[600],
+//                   letterSpacing: 2,
+//                   fontWeight: FontWeight.bold,
+//                   fontSize: 40,
+//                   fontFamily: 'Caveat',
+//                 ),
+//               ),
+//               Spacer(),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+// 
