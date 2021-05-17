acda280c41e01ee818dca997a2a65b646d224c06
KhushiShah01 <khushi.bharat.shah@gmail.com>
2021-05-17 14:35:56 +0530

    First screen

--- quillint/lib/main.dart
+++ quillint/lib/main.dart
@@ -10,10 +10,28 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('QUILLINT'),
+      backgroundColor: Colors.grey[900],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: [
+              SizedBox(height:250),
+              Text(
+                'QUILLINT',
+                style: TextStyle(
+                  color: Colors.amber[600],
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                  fontFamily: 'Caveat',
+                ),
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
       ),
-      body: Text('HOME'),
     );
   }
 }
